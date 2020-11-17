93e3b7e0b754dabf0c994e13875c8ce02ef5c6c8
Cormac Quaid <thequad@gmail.com>
2020-11-17 12:29:38 +0100

    Update to ISS crew details, added docked spacecraft info, formatting

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ iconSize: [70, 50],
 // Create ISS marker
 let marker = L.marker([-180,90], {
 icon: issIcon,
-title: 'International Space Station',
+title: 'International Space Station Position',
 alt: 'ISS icon'
 }).addTo(issMap);
 
@@ -46,7 +46,8 @@ alt: 'ISS icon'
 let circle = L.circle([-180, 90], {
   radius: 2300000,
   color: '#00E000',
-  opacity: 0.1
+  title: 'Visible ISS Horizon',
+  opacity: 0
 }).addTo(issMap);
 
 // Create Sun icon and marker
@@ -146,15 +147,17 @@ const centreMap = async() => {
 
 // =======================================================
 // People on ISS function
+// Spacecraft currently docked with ISS
 // National flags called from https://www.countryflags.io/
 // =======================================================
 
-const getCrew = () => {
+const issInfo = () => {
   
   const crew = [
     {
       name: 'Sergey Ryzhikov',
       country: 'Russia',
+      wiki: 'https://en.wikipedia.org/wiki/Sergey_Ryzhikov_(cosmonaut)',
       flag: 'https://www.countryflags.io/ru/shiny/64.png',
       position: 'Commander',
       launch: 1602633600
@@ -162,6 +165,7 @@ const getCrew = () => {
     {
       name: 'Sergey Kud-Sverchkov',
       country: 'Russia',
+      wiki: 'https://en.wikipedia.org/wiki/Sergey_Kud-Sverchkov',
       flag: 'https://www.countryflags.io/ru/shiny/64.png',
       position: 'Flight Engineer',
       launch: 1602633600
@@ -169,24 +173,119 @@ const getCrew = () => {
     {
       name: 'Kate Rubins',
       country: 'United States',
+      wiki: 'https://en.wikipedia.org/wiki/Kathleen_Rubins',
       flag: 'https://www.countryflags.io/us/shiny/64.png',
       position: 'Flight Engineer',
       launch: 1602633600
-    }
+    },
+    {
+      name: 'Mike Hopkins',
+      country: 'United States',
+      wiki: 'https://en.wikipedia.org/wiki/Michael_S._Hopkins',
+      flag: 'https://www.countryflags.io/us/shiny/64.png',
+      position: 'Flight Engineer',
+      launch: 1602633600
+    },
+    {
+      name: 'Victor Glover',
+      country: 'United States',
+      wiki: 'https://en.wikipedia.org/wiki/Victor_J._Glover',
+      flag: 'https://www.countryflags.io/us/shiny/64.png',
+      position: 'Flight Engineer',
+      launch: 1602633600
+    },
+    {
+      name: 'Shannon Walker',
+      country: 'United States',
+      wiki: 'https://en.wikipedia.org/wiki/Shannon_Walker',
+      flag: 'https://www.countryflags.io/us/shiny/64.png',
+      position: 'Flight Engineer',
+      launch: 1602633600
+    },
+    {
+      name: 'Soichi Noguchi',
+      country: 'Japan',
+      wiki: 'https://en.wikipedia.org/wiki/Soichi_Noguchi',
+      flag: 'https://www.countryflags.io/jp/shiny/64.png',
+      position: 'Flight Engineer',
+      launch: 1602633600
+    },
   ]
 
-  const expedition = 64;
+  const dockedSpacecraft = [
+    {
+      spacecraft: 'Progress MS-14',
+      wiki: 'https://en.wikipedia.org/wiki/Progress_MS-14',
+      dockDate: 'April 25th 2020',
+      flag: 'https://www.countryflags.io/ru/shiny/64.png'
+    },
+    {
+      spacecraft: 'Progress MS-15',
+      wiki: 'https://en.wikipedia.org/wiki/Progress_MS-15',
+      dockDate: 'July 23rd 2020',
+      flag: 'https://www.countryflags.io/ru/shiny/64.png'
+    },
+    {
+      spacecraft: 'Cygnus NG-14',
+      wiki: 'https://en.wikipedia.org/wiki/Cygnus_NG-14',
+      dockDate: 'Oct 5th 2020',
+      flag: 'https://www.countryflags.io/us/shiny/64.png'
+    },
+    {
+      spacecraft: 'Soyuz MS-17',
+      wiki: 'https://en.wikipedia.org/wiki/Soyuz_MS-17',
+      dockDate: 'Oct 14th 2020',
+      flag: 'https://www.countryflags.io/ru/shiny/64.png'
+    },
+    {
+      spacecraft: 'Crew-1 Dragon',
+      wiki: 'https://en.wikipedia.org/wiki/SpaceX_Crew-1',
+      dockDate: 'Nov 17th 2020',
+      flag: 'https://www.countryflags.io/us/shiny/64.png'
+    }
+  ];
 
-  document.getElementById("crew0").innerText = crew[0].name;
-  document.getElementById("crew1").innerText = crew[1].name;
-  document.getElementById("crew2").innerText = crew[2].name;
-  document.getElementById("img0").src = crew[0].flag;
-  document.getElementById("img1").src = crew[1].flag;
-  document.getElementById("img2").src = crew[2].flag;
+  const expedition = 64;
+  document.getElementById("crewname0").innerText = crew[0].name;
+  document.getElementById("crewname1").innerText = crew[1].name;
+  document.getElementById("crewname2").innerText = crew[2].name;
+  document.getElementById("crewname3").innerText = crew[3].name;
+  document.getElementById("crewname4").innerText = crew[4].name;
+  document.getElementById("crewname5").innerText = crew[5].name;
+  document.getElementById("crewname6").innerText = crew[6].name;
+  document.getElementById("crewimg0").src = crew[0].flag;
+  document.getElementById("crewimg1").src = crew[1].flag;
+  document.getElementById("crewimg2").src = crew[2].flag;
+  document.getElementById("crewimg3").src = crew[3].flag;
+  document.getElementById("crewimg4").src = crew[4].flag;
+  document.getElementById("crewimg5").src = crew[5].flag;
+  document.getElementById("crewimg6").src = crew[6].flag;
+  document.getElementById("crewwiki0").href = crew[0].wiki;
+  document.getElementById("crewwiki1").href = crew[1].wiki;
+  document.getElementById("crewwiki2").href = crew[2].wiki;
+  document.getElementById("crewwiki3").href = crew[3].wiki;
+  document.getElementById("crewwiki4").href = crew[4].wiki;
+  document.getElementById("crewwiki5").href = crew[5].wiki;
+  document.getElementById("crewwiki6").href = crew[6].wiki;
   document.getElementById("exp").innerText = expedition;
+  document.getElementById("craftname0").innerText = dockedSpacecraft[0].spacecraft;
+  document.getElementById("craftname1").innerText = dockedSpacecraft[1].spacecraft;
+  document.getElementById("craftname2").innerText = dockedSpacecraft[2].spacecraft;
+  document.getElementById("craftname3").innerText = dockedSpacecraft[3].spacecraft;
+  document.getElementById("craftname4").innerText = dockedSpacecraft[4].spacecraft;
+  document.getElementById("craftimg0").src = dockedSpacecraft[0].flag;
+  document.getElementById("craftimg1").src = dockedSpacecraft[1].flag;
+  document.getElementById("craftimg2").src = dockedSpacecraft[2].flag;
+  document.getElementById("craftimg3").src = dockedSpacecraft[3].flag;
+  document.getElementById("craftimg4").src = dockedSpacecraft[4].flag;
+  document.getElementById("craftwiki0").href = dockedSpacecraft[0].wiki;
+  document.getElementById("craftwiki1").href = dockedSpacecraft[1].wiki;
+  document.getElementById("craftwiki2").href = dockedSpacecraft[2].wiki;
+  document.getElementById("craftwiki3").href = dockedSpacecraft[3].wiki;
+  document.getElementById("craftwiki4").href = dockedSpacecraft[4].wiki;
 }
 
-getCrew();
+issInfo();
 
 
 
